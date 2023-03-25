707ab87af289a058208209ef7a714ff1b945834b
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2023-03-25 19:23:59 +0530

    updates changes

--- Acess Darshana react-native/FrontEnd/HBMS/App.js
+++ Acess Darshana react-native/FrontEnd/HBMS/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/HomeScreen";
+import Contact from "./components/Contact";
+import Map from "./components/Map";
+import complain from "./components/Complain";
+import Help_Support from "./components/Help_Support";
+
+import Faq from "./components/Faq";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen name="Help_Support" component={Help_Support} />
+        <Stack.Screen name="Complain" component={complain} />
+        
+        <Stack.Screen name="Faq" component={Faq} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
